2c5549a3d3cccce5873a94a6d73a7498d3f30f76
KillzXGaming <millernathan35@gmail.com>
2020-07-05 18:50:14 -0400

    Remove unecssary reference.

--- Toolbox.Winforms/Forms/ImageEditor/ImageEditorBase.cs
+++ Toolbox.Winforms/Forms/ImageEditor/ImageEditorBase.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Activities.Statements;
 using System.Threading;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
